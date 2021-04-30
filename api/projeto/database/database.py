0ad732797563793e0d7219dac504a25623706899
C1281149 Lucas Dynczuki Palla <c1281149@interno.bb.com.br>
2021-04-29 23:55:07 -0300

    Ajustes instância MongoDB

--- api/projeto/database/database.py
+++ api/projeto/database/database.py
@@ -4,20 +4,18 @@ from projeto.settings import MONGODB_URL, MONGODB_DATABASE, MONGODB_COLLECTION
 
 class Iris():
 
+    database = MongoDataBase(MONGODB_URL, MONGODB_DATABASE, MONGODB_COLLECTION)
+
     def listar(self):
         
-        database = MongoDataBase(MONGODB_URL, MONGODB_DATABASE, MONGODB_COLLECTION)
-        response = database.read_all()
-
+        response = self.database.read_all()
         return response
     
 
     def inserir(self, sepal_length, sepal_width, 
                 petal_length, petal_width, response_clf):
 
-        database = MongoDataBase(MONGODB_URL, MONGODB_DATABASE, MONGODB_COLLECTION)
-        response = database.create({'sepal_length': sepal_length, 'sepal_width': sepal_width, 
+        response = self.database.create({'sepal_length': sepal_length, 'sepal_width': sepal_width, 
                                     'petal_length': petal_length, 'petal_width': petal_width, 
                                     'classe': response_clf})
-
         return response
